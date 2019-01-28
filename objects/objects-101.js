202b133cd495318fe001a99c55ced42753ffacc2
Mindysueasaurus <mymindyyeager@gmail.com>
2019-01-28 15:01:24 -0600

    objects-101

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -1,3 +1,26 @@
 let myBook = {
-   
+   title: "1984",
+   author: 'George Orwell',
+   pageCount: 326
 }
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+//challenge
+//Model a person: name, age, location
+//Make a string using the values
+
+let person = {
+  name: 'Mindy',
+  age: 37,
+  location: 'San Antonio'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age += 1
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
