0b7dedcf4a4a68ab05d2cfa2bc11dd13c0ad201d
Mindysueasaurus <mymindyyeager@gmail.com>
2019-02-05 19:20:06 -0600

    work with array's

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -31,20 +31,55 @@ const notes = [{
 //   console.log(notes[count])
 // } 
 
-const findNote = function(notes, noteTitle){
-  const index = notes.findIndex(function(note, index) {
-    return note.title === noteTitle
+// const findNote = function(notes, noteTitle){
+//   const index = notes.findIndex(function(note, index) {
+//     return note.title === noteTitle
+//   })
+// }
+//  const findNote = function(notes, noteTitle) {
+//   const index = notes.findIndex(function(note, index){
+//     return note.title.toLowerCase() === noteTitle.toLowerCase()
+//   })
+//   return notes[index]
+//  }
+// const findNote = function(notes, noteTitle) {
+//   return notes.find(function(note, index){
+//     return note.title.toLowerCase() === noteTitle.toLowerCase()
+//   })
+//  }
+// console.log(notes.length)
+// const findNotes = function (notes, query){
+//     return filteredNotes = notes.filter(function(note, index) {
+//     const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
+//     const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
+//     return isTitleMatch || isBodyMatch
+//   })
+// }
+
+
+// console.log(findNotes(notes, 'to'))
+
+// const note = findNote(notes, 'ton Practice a new Language')
+// console.log(note)
+
+// const index = notes.findIndex(function(note, index){
+//   console.log(note)
+//   return note.title === 'Habits to work on'
+// })
+
+// console.log(index)
+
+const sortNotes = function(notes) {
+  notes.sort(function(a, b){
+    if(a.title.toLowerCase() < b.title.toLowerCase()){
+      return -1
+    } else if (b.title.toLowerCase() < a.title.toLowerCase()){
+      return 1
+    } else {
+      return 0
+    }
   })
 }
 
-
-console.log(notes.length)
-
+sortNotes(notes)
 console.log(notes)
-
-const index = notes.findIndex(function(note, index){
-  console.log(note)
-  return note.title === 'Habits to work on'
-})
-
-console.log(index)
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,23 +1,67 @@
-const todo = ['Walk the dog', 'Feed the cats', 'Go to Walmart', 'Pick up prescriptions', 'Call mom']
-const todoCount  = todo.length 
+// const todo = ['Walk the dog', 'Feed the cats', 'Go to Walmart', 'Pick up prescriptions', 'Call mom']
+const todos = [{
+  text:'Walk the dog', 
+  completed: false
+},
+{
+  text:'Feed the cats',
+  completed: false
+} , 
+{
+  text: 'Go to Walmart',
+  completed: true
+ },
+ {
+   text:'Pick up prescriptions',
+   completed: true
+ } , 
+ {
+   text: 'Call mom',
+   completed: false
+ }]
+// const todoCount  = todo.length 
 
-console.log('You have', todoCount, 'todos')
-console.log('Todo:', todo[0])
+// console.log('You have', todoCount, 'todos')
+// console.log('Todo:', todo[0])
 
-console.log('Todo:', todo[todo.length - 2])
+// console.log('Todo:', todo[todo.length - 2])
 
-console.log(todo.splice(2, 1))
-console.log(todo.push('This is my new todo'))
-console.log(todo)
-console.log(todo.shift())
-console.log(todo)
+// console.log(todo.splice(2, 1))
+// console.log(todo.push('This is my new todo'))
+// console.log(todo)
+// console.log(todo.shift())
+// console.log(todo)
 
-todo.forEach(function(item,index){
-  console.log(`${index + 1}. ${item}`)
-})
+// todo.forEach(function(item,index){
+//   console.log(`${index + 1}. ${item}`)
+// })
 
-for(let count = 0; count < todo.length; count++){
-  const num = count + 1
-  const todos = todo[count]
-  console.log(`${num}. ${todos}`)
+// for(let count = 0; count < todo.length; count++){
+//   const num = count + 1
+//   const todos = todo[count]
+//   console.log(`${num}. ${todos}`)
+// }
+
+//convert array to array of objects -> Text, completed
+//create a function to remove a todo by text value
+
+const deleteTodo = function(todos, text) {
+  const index = todos.findIndex(function(todo ){
+    return todo.text.toLowerCase() === text.toLowerCase()
+  })
+  if (index > -1) {
+    todos.splice(index, 1)
+  }
 }
+
+const getThingsToDo = function(todos){
+  return todos.filter(function(todo) {
+     return !todo.completed 
+    
+  })
+
+}
+
+console.log(getThingsToDo(todos))
+// deleteTodo(todos,'Feed the cats')
+// console.log(todos)
